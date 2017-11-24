8139e9c526dce126431075720ecc443712bb5bca
janpisl <janpisl@hotmail.com>
2017-11-24 13:04:07 +0100

    minor edits, code cleaned up; see #5

--- src/processes/dbtest.py
+++ src/processes/dbtest.py
@@ -12,17 +12,13 @@ from pywps.validator.mode import MODE
 
 class DbTest(Process):
     def __init__(self):
-        inputs=[LiteralInput('dbname', 'Input name of a database', data_type='string'),
+        inputs=[LiteralInput('db_section', 'Input name of a database', data_type='string'),
                 ComplexInput('poly_in', 'Input vector file',
                 supported_formats=[Format('application/gml+xml')],
                 mode=MODE.STRICT),
                 LiteralInput('buffer', 'Buffer size', data_type='float',
                 allowed_values=(0, 1, 10, (10, 10, 100), (100, 100, 1000)))
         ]
-
-        # outputs=[ComplexOutput('buff_out', 'Buffered file',
-        #                      supported_formats=[Format('application/gml+xml')])
-        # ]
         outputs=[LiteralOutput('buff_out', 'Buffered table', data_type='string')
         ]
         
@@ -39,44 +35,11 @@ class DbTest(Process):
             status_supported=True
         )
 
-    def unique_schema(self):
-        schema_name = '{}_{}'.format(self.identifier.lower(),
-                            str(self.uuid).replace("-", "_").lower()
-                            )
-        connstr = self.dbconnect()
-        conn = psycopg2.connect(connstr)
-        cur = conn.cursor()
-        query = 'CREATE SCHEMA IF NOT EXISTS {};'.format(schema_name) # must be saved as a variable. if used directly, throws an error: TypeError: not all arguments converted during string formatting' 
-        cur.execute(query)
-        conn.commit()
-        cur.close()
-        conn.close()
-        return schema_name
-
-    def dbconnect(self):
-        return "dbname={} user={} password={} host={}".format(
-            self.user_input,
-            get_config_value("db" , "user"), 
-            get_config_value("db" , "password"),
-            get_config_value("db" , "host"),
-        )
-
-    def store_output_db(self, layer):
-        connstr = self.dbconnect()
-        #        try:
-        logging.debug("Connect string: {}".format(connstr))
-        dsc = ogr.Open("PG:" + connstr)
-        if dsc is None:
-            raise Exception("Database connection has not been established.")
-        layer = dsc.CopyLayer(layer, "buff_out", ['OVERWRITE=YES',
-                                                  'SCHEMA={}'.format(self.unique_schema())]
-        )
-        # TODO: layer is valid even copying failed (schema do not exists)
-        if layer is None:
-            raise Exception("Writing output data to database failed.")
-
     def _handler(self, request, response):
-        self.user_input= request.inputs['dbname'][0].data    #co znamena [0]?
+        self.user_input= request.inputs['db_section'][0].data
+        
+        self.setOutputDbStorage(self.user_input)
+        
         inSource = ogr.Open(request.inputs['poly_in'][0].file)
         inLayer = inSource.GetLayer()
         out = inLayer.GetName() + '_buffer'
@@ -110,13 +73,6 @@ class DbTest(Process):
             response.update_status('Buffering', 100*(index/featureCount))
 
         outSource.Destroy()
-
-        for param in response.outputs.keys():
-            outSource = ogr.Open(out) # ???
-            self.store_output_db(outSource.GetLayer())
-            outSource.Destroy()
         
-            #        response.outputs['buff_out'].output_format = FORMATS.GML
-            response.outputs[param].data = '{}.{}.{}'.format(self.user_input, self.unique_schema(), param)
-
+        response.outputs["buff_out"].file = out  
         return response
