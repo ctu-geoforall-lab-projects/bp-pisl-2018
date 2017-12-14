2f9c435c49d013e4bab91353b97ae5d8a3205df8
Martin Landa <landa.martin@gmail.com>
2017-12-14 15:47:09 +0100

    rewrite tests in loop, check refcount + feature type in TODO

--- src/test.py
+++ src/test.py
@@ -1,40 +1,76 @@
+#!/usr/bin/env python3
+
 import requests
 import xml.etree.ElementTree as ET
 
-while True:
-    process_number = int(input("Please enter 0 for process-no-output, 1 for process-one-output and 2 for process-two-outputs: "))
-    if process_number == 0:
-        URL = "http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-no-output&datainputs=name=xxx"
-        break
-    elif process_number == 1:
-        URL = "http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-one-output&datainputs=db_section=db;poly_in=@xlink:href=http://127.0.0.1:5000/static/data/points.gml;buffer=10"
-        break
-    elif process_number == 2:
-        URL = "http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-two-outputs&datainputs=db_section=db;poly_in=@xlink:href=http://127.0.0.1:5000/static/data/points.gml;buffer=10"
-        break
-    else:
-        print("Input Error")
+def parse_string(value):
+    if len(value.split(sep=".")) != 3:
+        raise Exception("Table name does not consist of 3 parts separated by a dot.")
 
-response = requests.get(URL)
-tree = ET.ElementTree(response)
-root = ET.fromstring(response.content)
+def get_value(node, idx=0):
+    try:
+        value = node[2][idx][2][0].text
+    except IndexError:
+        raise Exception("No output value found (process probably failed)")
 
-identifier = root[0][0].text
-string = root[2][0][2][0].text
+    if type(value) != str:
+        raise Exception("Output value is not a string.")
 
-if type(string) != str:
-    raise Exception("Not a string.")
+    return value
+
+def get_connstr():
+    pass # PG:dbname=...
+
+def check_output(value, refcount, reftype):
+    db, idsch, table = value.split('.')
+    ident, schema = idsch.split('_')
+
+    dsn = ogr.Open(get_connstr())
+    if dsn is None:
+        raise ...
+    lyr = dsn.GetLayerByName('{}.{}'.format(schema, table))
+    if lyr is None:
+        raise ...
+    count = lyr.GetFeatureCount()
+    if count != refcount:
+        raise None
+    # type vs reftype
+
+def get_refcount(uri):
+    dsn = ogr.Open('/vsicurl/{}'.format(data))
+    ...
+    lyr = dsn.GetLayer()
+    ..
+
+    return lyr.GetFeatureCount()
     
-if identifier != 'process-no-output':
-    table1_name =(string.split(sep="."))
-    if len(table1_name) != 3:
-        raise Exception("Buffered table name does not consist of 3 parts separated by a dot.")
+def run_process(URL, refcount):
+    response = requests.get(URL)
+    tree = ET.ElementTree(response)
+    root = ET.fromstring(response.content)
+
+    identifier = root[0][0].text
+    print ("Process: {}".format(identifier))
+    
+    value = get_value(root)
+
+    if identifier != 'process-no-output':
+        parse_string(value)
+        check_output(value, refcount, ogr.wkbPolygon)
+        
     if identifier == 'process-two-outputs':
-        string2 = root[2][1][2][0].text
-        table2_name = (string2.split(sep="."))
-        if len(table2_name) != 3:
-            raise Exception("Centroids table name does not consist of 3 parts separated by a dot.")
+        value2 = get_value(root, idx=1)
+        parse_string(value2)
+        check_output(value, refcount, ogr.wkbPoint)
 
+if __name__ == "__main__":
+    data = "http://127.0.0.1:5000/static/data/points.gml"
 
+    refcount = get_refcount(data)
+    
+    for URL in ["http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-no-output&datainputs=name=xxx",
+                "http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-one-output&datainputs=db_section=db;poly_in=@xlink:href={};buffer=10".format(data),
+                "http://127.0.0.1:5000/wps?service=wps&&version=1.0.0&request=execute&identifier=process-two-outputs&datainputs=db_section=db;poly_in=@xlink:href={};buffer=10".format(data)]:
+        run_process(URL, refcount)
 
 
